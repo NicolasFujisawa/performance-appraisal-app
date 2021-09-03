4ebe698f4334e21e002f2192d07e25e58f6df9ec
jasondavindev <jasondavin.dev@gmail.com>
2021-09-03 20:59:39 -0300

    create rest of entities

--- backend/src/entity/student.entity.ts
+++ backend/src/entity/student.entity.ts
@@ -1,6 +1,8 @@
+import { Evaluation } from '@/entity/evaluation.entity';
 import { TeamMember } from '@/entity/teamMember.entity';
 import { IsNotEmpty } from 'class-validator';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Score } from './score.entity';
 
 @Entity()
 export class Student {
@@ -17,4 +19,10 @@ export class Student {
 
   @OneToMany(() => TeamMember, member => member.student)
   teams: TeamMember[];
+
+  @OneToMany(() => Score, score => score.evaluatorStudent)
+  evaluationsMade: Evaluation[];
+
+  @OneToMany(() => Score, score => score.evaluatedStudent)
+  evaluationsReceived: Evaluation[];
 }
