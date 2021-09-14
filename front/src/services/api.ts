c0477455a5031d5e76e2368f66433cae34587cb6
jasondavindev <jasondavin.dev@gmail.com>
2021-09-13 21:48:49 -0300

    add api integration

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,7 +1,54 @@
 import axios from 'axios'
+import { CriteriasResponse } from '../interfaces/criterias.response'
+import { SendEvaluationScoresPayload } from '../interfaces/send.evaluation.scores.payload'
 
 const instance = axios.create({
-  baseURL: 'https://localhost:8080/',
+  baseURL: 'http://localhost:8080/',
 })
+
+export const sendEvaluationScores = async (
+  payload: SendEvaluationScoresPayload
+) => instance.post('/scores', payload)
+
+export const getCriterias = async (
+  teamId: number
+): Promise<CriteriasResponse> => {
+  return [
+    {
+      criteriaId: 1,
+      name: 'Proatividade',
+      methodId: 1,
+      citeriaScores: [
+        {
+          citeriaScoreId: 1,
+          name: 'Proativo',
+          value: 3,
+        },
+        {
+          citeriaScoreId: 2,
+          name: 'Folgado',
+          value: 0,
+        },
+      ],
+    },
+    {
+      criteriaId: 2,
+      name: 'Autonomia',
+      methodId: 2,
+      citeriaScores: [
+        {
+          citeriaScoreId: 3,
+          name: 'Autonomo',
+          value: 3,
+        },
+        {
+          citeriaScoreId: 4,
+          name: 'CLT',
+          value: 0,
+        },
+      ],
+    },
+  ]
+}
 
 export default instance
