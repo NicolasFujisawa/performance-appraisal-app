bd7f7e9e122f7a067a7660431a9bbdb4a90cf7c3
Leonardo Lopes <leonardo.lopes45@gmail.com>
2021-09-16 21:09:29 -0300

    Create results page

---
+++ front/src/pages/Results.tsx
@@ -0,0 +1,17 @@
+import '../styles/pages/main-page.css'
+
+export default function Results() {
+  return (
+    <div id="page-container">
+      <h1>Avaliação</h1>
+      <h4>03/12/2021 - Finished</h4>
+      <h4>Hadouken Team</h4>
+      <br />
+      <h1>Resultados</h1>
+      <h5>Vestibulum (aluno)</h5>
+      <p>Criterio 1:3</p>
+      <p>Criterio 2:2</p>
+      <p>Criterio 3:3</p>
+    </div>
+  )
+}
--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Evaluation from './pages/Evaluation'
+import Results from './pages/Results'
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/evaluation" component={Evaluation} />
+        <Route path="/results" component={Results} />
       </Switch>
     </BrowserRouter>
   )
