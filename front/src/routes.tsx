a53c5edd4a274d2f6002db03d2bcca97f5630982
Leonardo Lopes <leonardo.lopes45@gmail.com>
2021-09-19 14:22:11 -0300

    Removing unecessary lines

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -6,7 +6,6 @@ function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/evaluation" component={Evaluation} />
         <Route path="/results" component={Results} />
         <Route path="/evaluation/:id" component={Evaluation} />
       </Switch>
