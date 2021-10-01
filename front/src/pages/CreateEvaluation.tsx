30278569c086d06e80b6a210dd3033665b0e80f8
Leonardo Lopes <leonardo.lopes45@gmail.com>
2021-10-01 19:42:12 -0300

    Adjust form

--- front/src/pages/CreateEvaluation.tsx
+++ front/src/pages/CreateEvaluation.tsx
@@ -5,18 +5,23 @@ export default function Evaluation() {
     <div id="page-container">
       <h1>Dados</h1>
       <form>
-        <p>Nome</p>
-        <input type="text" />
-        <p>Critérios</p>
-        <select>
-          <option value="1">Pacer</option>
-          <option value="2">Critério Simples</option>
-        </select>
-        <p>Manter Anonimato?</p>
-        <input type="radio" value="1" name="anonymous" />
-        <label>Sim</label>
-        <input type="radio" value="2" name="anonymous" checked />
-        <label>Não</label>
+        <div>
+          <p>Nome</p>
+          <input type="text" />
+          <p>Critérios</p>
+          <select>
+            <option value="1">Pacer</option>
+            <option value="2">Critério Simples</option>
+          </select>
+          <p>Manter Anonimato?</p>
+          <input type="radio" value="1" name="anonymous" />
+          <label>Sim</label>
+          <input type="radio" value="2" name="anonymous" checked />
+          <label>Não</label>
+        </div>
+        <button className="confirm-button" type="submit">
+          Próximo
+        </button>
       </form>
     </div>
   )
