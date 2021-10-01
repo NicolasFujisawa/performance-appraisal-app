d3a8279afc9cc17440b8b7c07c09b62cab61331f
Leonardo Lopes <leonardo.lopes45@gmail.com>
2021-10-01 18:47:38 -0300

    Create Evaluation page

---
+++ front/src/pages/CreateEvaluation.tsx
@@ -0,0 +1,23 @@
+import '../styles/pages/main-page.css'
+
+export default function Evaluation() {
+  return (
+    <div id="page-container">
+      <h1>Dados</h1>
+      <form>
+        <p>Nome</p>
+        <input type="text" />
+        <p>Critérios</p>
+        <select>
+          <option value="1">Pacer</option>
+          <option value="2">Critério Simples</option>
+        </select>
+        <p>Manter Anonimato?</p>
+        <input type="radio" value="1" name="anonymous" />
+        <label>Sim</label>
+        <input type="radio" value="2" name="anonymous" checked />
+        <label>Não</label>
+      </form>
+    </div>
+  )
+}
--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Evaluation from './pages/Evaluation'
 import Results from './pages/Results'
+import CreateEvaluation from './pages/CreateEvaluation'
 
 function Routes() {
   return (
@@ -8,6 +9,7 @@ function Routes() {
       <Switch>
         <Route path="/results" component={Results} />
         <Route path="/evaluation/:id" component={Evaluation} />
+        <Route path="/create_evaluation" component={CreateEvaluation} />
       </Switch>
     </BrowserRouter>
   )
