b4154fbbeacbbb501814d861b3caa1951d31aaac
jasondavindev <jasondavin.dev@gmail.com>
2021-10-01 19:46:48 -0300

    create method route

---
+++ backend/src/services/methods.service.ts
@@ -0,0 +1,21 @@
+import { CreateMethodDto } from '@/dtos/create.method.dto';
+import { Method } from '@/entity/method.entity';
+import { getRepository, Repository } from 'typeorm';
+
+class MethodsService {
+  public method = Method;
+  private _methodRepository: Repository<Method>;
+
+  get methodRepository() {
+    if (this._methodRepository) return this._methodRepository;
+
+    this._methodRepository = getRepository(this.method);
+    return this._methodRepository;
+  }
+
+  public async create(payload: CreateMethodDto) {
+    return this.methodRepository.save(payload);
+  }
+}
+
+export default MethodsService;
