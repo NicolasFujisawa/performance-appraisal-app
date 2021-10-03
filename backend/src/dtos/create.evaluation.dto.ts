07fb5f12cb3233c34fd54ca8753b04b53e416bdb
Nicolas Fernando Espinoza Fujisawa <50681635+NicolasFujisawa@users.noreply.github.com>
2021-10-03 06:58:24 -0300

    fix: add name in evaluation dto (#29)

--- backend/src/dtos/create.evaluation.dto.ts
+++ backend/src/dtos/create.evaluation.dto.ts
@@ -2,6 +2,7 @@ import { Method } from '@/entity/method.entity';
 import { Team } from '@/entity/team.entity';
 
 export class CreateEvaluationDto {
+  name: string;
   start: string;
   end: string;
   isLastEvaluation: boolean;
