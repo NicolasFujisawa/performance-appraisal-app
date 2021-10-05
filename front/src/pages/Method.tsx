d1371bb76721ba4c3f1b7d851c44a2ff0934ea5e
toledompm <toledompm@gmail.com>
2021-10-04 21:16:01 -0300

    feat: css

--- front/src/pages/Method.tsx
+++ front/src/pages/Method.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
+import DeleteButton from '../commons/components/DeleteButton'
 import { CriteriaResponse } from '../interfaces/criterias.response'
 import '../styles/pages/main-page.css'
 import '../styles/pages/method-page.css'
 
-
 const mockCriterias: CriteriaResponse[] = [
-  {} as any,
+  {} as CriteriaResponse,
   {
     criteriaId: 1,
     name: 'Criteria 1',
@@ -21,76 +21,91 @@ const mockCriterias: CriteriaResponse[] = [
     name: 'Criteria 3',
     criteriaScores: [],
   },
-];
+]
 
 export default function Method() {
-  const [criterias, setCriterias] = useState<Array<CriteriaResponse>>(mockCriterias)
-  const [chosenCriterias, setChosenCriterias] = useState<Array<CriteriaResponse>>(
-    []
-  )
+  const [criterias, setCriterias] =
+    useState<Array<CriteriaResponse>>(mockCriterias)
+  const [chosenCriterias, setChosenCriterias] = useState<
+    Array<CriteriaResponse>
+  >([])
+  const [name, setName] = useState('')
 
+  // nao gostou muda
   const handleChange = (e: any) => {
     const { value } = e.target
 
-    const [newChosenCriteria] = criterias.filter(criteria => criteria.name === value)
-    const newCriterias = criterias.filter(
-      (criteria) => criteria.name !== value
+    const newChosenCriteria = criterias.find(
+      (criteria) => criteria.name === value
     )
+    const newCriterias = criterias.filter((criteria) => criteria.name !== value)
 
     setCriterias(newCriterias)
-    setChosenCriterias([...chosenCriterias, newChosenCriteria])
+    setChosenCriterias([...chosenCriterias, newChosenCriteria!])
   }
 
-  const handleRemove = (criteria: CriteriaResponse) => {
-    const newCriterias = [...criterias, criteria]
+  const handleRemove = (criteriaId: number) => {
+    const criteriaToRemove = chosenCriterias.find(
+      (criteria) => criteria.criteriaId === criteriaId
+    )
+    const newCriterias = [...criterias, criteriaToRemove!]
     const newChosenCriterias = chosenCriterias.filter(
-      (chosenCriteria) => chosenCriteria !== criteria
+      (chosenCriteria) => chosenCriteria !== criteriaToRemove
     )
 
     setCriterias(newCriterias)
     setChosenCriterias(newChosenCriterias)
   }
 
+  const handleSubmit = () => {}
 
   return (
     <div id="page-component">
       <main>
-        <div id="page-container">
-          <h1>Dados</h1>
-          <br />
-          <form>
-            <label>
-              Nome
-              <br />
-              <input className="drop-down" type="text" name="name" />
-            </label>
-            <br />
-            <label>
-              Critérios
-              <br />
+        <form id="page-container" onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Dados</legend>
+            <div className="input-block">
+              <label htmlFor="name">Nome</label>
+              <div className="method-name-input">
+              <input
+                id="name"
+                placeholder="Nome do Método"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+              </div>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="name">Critérios</label>
               <select className="drop-down" value={''} onChange={handleChange}>
                 {criterias.map((criteria: CriteriaResponse) => (
-                  <option key={criteria.criteriaId} value={criteria.name}>
+                  <option key={criteria.criteriaId} value={criteria.name} className="drop-down-option">
                     {criteria.name}
                   </option>
                 ))}
               </select>
-            </label>
-            {chosenCriterias.map((criteria: CriteriaResponse) => (
-              <div key={criteria.criteriaId}>
-                <label>
-                  {criteria.name}
-                  <button onClick={() => handleRemove(criteria)}>x</button>
-                </label>
-              </div>
-            ))}
-            <br />
-            <br />
+            </div>
+
+            <div className="criteria-list">
+              {chosenCriterias.map((criteria: CriteriaResponse) => (
+                <div className="criteria-item" key={criteria.criteriaId}>
+                  <div className="item-name">
+                    <p>{criteria.name}</p>
+                  </div>
+                  <DeleteButton
+                    action={handleRemove}
+                    id={criteria.criteriaId}
+                  />
+                </div>
+              ))}
+            </div>
             <button className="confirm-button" type="submit">
               Confirmar
             </button>
-          </form>
-        </div>
+          </fieldset>
+        </form>
       </main>
     </div>
   )
