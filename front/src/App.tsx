9782752a2a62d1ee99fc994640d8d49c6584790d
Jason Carneiro <jasondavin.dev@gmail.com>
2021-11-05 19:07:18 -0300

    integrates login page and backend (#44)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,7 @@
 import Routes from './routes'
-import { useAppDispatch } from './store/hooks'
-import { logIn } from './store/slices/userSlice'
 import './styles/global.css'
 
 function App() {
-  useAppDispatch()(logIn({ userId: 1, role: 'teacher' }))
   return <Routes />
 }
 
--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,18 +1,57 @@
-import '../styles/pages/login.css'
+import { FormEvent, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import loginImage from '../images/login.png'
+import { authLogin } from '../services/api'
+import { useAppDispatch } from '../store/hooks'
+import { logIn } from '../store/slices/userSlice'
+import '../styles/pages/login.css'
 
 export function Login() {
+  const dispatch = useAppDispatch()
+  const history = useHistory()
+
+  const [userIdentifier, setUserIdentifier] = useState<string>('')
+  const [invalidCredentials, setInvalidCredentials] = useState<boolean>(false)
+
+  const userIdentifierOnChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setUserIdentifier(event.target.value)
+
+  const handleLogin = async (event: FormEvent) => {
+    event.preventDefault()
+
+    const payload = { identifier: userIdentifier }
+
+    try {
+      const {
+        data: { data },
+      } = await authLogin(payload)
+      dispatch(logIn(data))
+      history.push('/teams')
+    } catch (error) {
+      setInvalidCredentials(true)
+    }
+  }
+
   return (
     <div className="login-page">
       <div>
         <img src={loginImage} className="image-login" />
       </div>
-      <form id="form-login">
+      <form id="form-login" onSubmit={handleLogin}>
         <div className="input-block">
-          <input type="text" placeholder="Login"></input>
-        </div>
-        <div className="input-block">
-          <input type="password" placeholder="Senha"></input>
+          {invalidCredentials ? (
+            <div id="invalid-credentials">
+              <p>Esse login é inválido.</p>
+            </div>
+          ) : (
+            ''
+          )}
+          <input
+            type="text"
+            placeholder="Login"
+            value={userIdentifier}
+            onChange={userIdentifierOnChange}
+          ></input>
         </div>
         <button type="submit" className="button-login">
           Entrar
