e5f1ba9902d885a355e8bdf9e2cbf74bb77d1212
NicolasFujisawa <nicolas.fujisawa@fatec.sp.gov.br>
2021-11-25 00:59:20 -0300

    Add persistence tin redux state

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,12 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {
+  AnyAction,
+  combineReducers,
+  configureStore,
+  Reducer,
+} from '@reduxjs/toolkit'
+import { persistReducer } from 'reduxjs-toolkit-persist'
+import autoMergeLevel1 from 'reduxjs-toolkit-persist/es/stateReconciler/autoMergeLevel1'
+import persistStore from 'reduxjs-toolkit-persist/lib/persistStore'
+import storage from 'reduxjs-toolkit-persist/lib/storage'
 import userSlice from './slices/userSlice'
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel1,
+}
+
+const reducers: Reducer<any, AnyAction> = combineReducers({
+  user: userSlice,
+})
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
+  reducer: persistedReducer,
 })
 
 export default store
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
