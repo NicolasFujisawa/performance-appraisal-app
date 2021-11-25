3d8555bfdc59c4b1fc10029d65756b9dda0aa0e3
Nicolas Fernando Espinoza Fujisawa <50681635+NicolasFujisawa@users.noreply.github.com>
2021-11-25 19:29:10 -0300

    Add persistence tin redux state (#59)

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'reduxjs-toolkit-persist/integration/react'
 import App from './App'
-import store from './store/store'
+import store, { persistor } from './store/store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
